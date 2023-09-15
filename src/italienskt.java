4822eff3d6ebeeecf92e4fbfc718f87bbabc059d
Mohsen Gholami <gholamimohsen1999@gmail.com>
2023-09-15 13:01:23 +0200

    flingsalt

--- src/italienskt.java
+++ src/italienskt.java
@@ -2,6 +2,7 @@ public class italienskt {
 
     public static void main(String[] args) {
         System.out.println("8 kycklinglårfiléer, med skinn");
+        System.out.println("flingsalt");
     }
 
 }
