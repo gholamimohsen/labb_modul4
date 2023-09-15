b9aea788e1d0886433727b1192a52a453fcee5da
Fazilet1820 <senolfazilet1820@gmail.com>
2023-09-15 13:01:19 +0200

    1 purjolök

--- src/zucchini.java
+++ src/zucchini.java
@@ -1,5 +1,8 @@
 public class zucchini {
     public static void main(String[] args) {
         System.out.println("800 gr");
+        System.out.println("1 purjolök\n" +
+                "6 ägg\n" +
+                "2 dl vetemjöl\n");
     }
 }
